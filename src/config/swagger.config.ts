c2cadde7963da1d93e85f6f7d9db3e3b8ae2c0ee
Lankiman <marvellousgboun@gmail.com>
2026-03-12 16:59:36 +0100

    fix:fixed listed bugs

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,12 +1,13 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { API_PREFIX } from './constants';
+import env from './env.config';
 
 export default function setUpSwagger(app: INestApplication): void {
   const options = new DocumentBuilder()
-    .setTitle('Ndu-TV')
+    .setTitle(env.APP_NAME)
     .setVersion(API_PREFIX)
-    .setDescription('Documentation for Ndu-TV server')
+    .setDescription(`Documentation for ${env.APP_NAME} server`)
     .addCookieAuth('access_token')
     .build();
 
